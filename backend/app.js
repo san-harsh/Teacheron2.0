0412acc8577f277ab64952b655578c55c4accc08
san-harsh <40207771+san-harsh@users.noreply.github.com>
2022-12-05 10:38:56 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+
 const errorMiddleware = require("./middleware/error");
 app.use(express.json());
+app.use(cookieParser());
 
 //Router Imports
 
-const product = require("./routes/jobRoutes");
-
-app.use("/api/v1", product);
+const job = require("./routes/jobRoutes");
+const user = require("./routes/userRoutes");
+app.use("/api/v1", user);
+app.use("/api/v1", job);
 
 //Middleware for the error
 app.use(errorMiddleware);
